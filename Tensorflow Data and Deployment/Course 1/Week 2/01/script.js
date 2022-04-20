12bbdb4688add2b1460cb0751a3243bcbad7ee4e
rifqoi <alfurqon.rifqi@gmail.com>
2022-04-21 01:28:32 +0700

    Add tfjs deployment week2

---
+++ Tensorflow Data and Deployment/Course 1/Week 2/01/script.js
@@ -0,0 +1,131 @@
+import { MnistData } from "./data.js";
+var canvas, ctx, saveButton, clearButton;
+var pos = { x: 0, y: 0 };
+var rawImage;
+var model;
+
+const getModel = () => {
+  const model = tf.sequential();
+  model.add(
+    tf.layers.conv2d({
+      inputShape: [28, 28, 1],
+      kernelSize: 3,
+      filters: 8,
+      activation: "relu",
+    })
+  );
+
+  model.add(tf.layers.maxPooling2d({ poolSize: [2, 2] }));
+  model.add(
+    tf.layers.conv2d({
+      inputShape: [28, 28, 1],
+      kernelSize: 3,
+      filters: 16,
+      activation: "relu",
+    })
+  );
+  model.add(tf.layers.maxPooling2d({ poolSize: [2, 2] }));
+
+  model.add(tf.layers.flatten());
+  model.add(
+    tf.layers.dense({
+      units: 128,
+      activation: "relu",
+    })
+  );
+
+  model.add(tf.layers.dense({ units: 10, activation: "softmax" }));
+
+  model.compile({
+    loss: "categoricalCrossentropy",
+    optimizer: tf.train.adam(),
+    metrics: ["accuracy"],
+  });
+
+  return model;
+};
+
+const train = async (model, data) => {
+  const metrics = ["loss", "val_loss", "acc", "val_acc"];
+  const container = { name: "Model Training", styles: { height: "640px" } };
+  const fitCallbacks = tfvis.show.fitCallbacks(container, metrics);
+
+  const BATCH_SIZE = 512;
+  const TRAIN_DATA_SIZE = 5500;
+  const TEST_DATA_SIZE = 1000;
+
+  const [trainXs, trainYs] = tf.tidy(() => {
+    const d = data.nextTrainBatch(TRAIN_DATA_SIZE);
+    return [d.xs.reshape([TRAIN_DATA_SIZE, 28, 28, 1]), d.labels];
+  });
+
+  const [testXs, testYs] = tf.tidy(() => {
+    const d = data.nextTestBatch(TEST_DATA_SIZE);
+    return [d.xs.reshape([TEST_DATA_SIZE, 28, 28, 1]), d.labels];
+  });
+
+  return model.fit(trainXs, trainYs, {
+    batchSize: BATCH_SIZE,
+    validationData: [testXs, testYs],
+    epochs: 20,
+    shuffle: true,
+    callback: fitCallbacks,
+  });
+};
+
+const setPosition = (e) => {
+  pos.x = e.clientX - 100;
+  pos.y = e.clientY - 100;
+};
+
+const draw = (e) => {
+  if (e.buttons != 1) return;
+  ctx.beginPath;
+  ctx.lineWidth = 24;
+  ctx.lineCap = "round";
+  ctx.strokeStyle = "white";
+  ctx.moveTo(pos.x, pos.y);
+  setPosition(e);
+  ctx.lineTo(pos.x, pos.y);
+  ctx.stroke();
+  rawImage.src = canvas.toDataURL("image/png");
+};
+
+const erase = () => {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, 280, 280);
+};
+
+const save = () => {
+  setPosition(e);
+  ctx.lineTo(pos.x, pos.y);
+  ctx.stroke();
+  rawImage.src = canvas.toDataURL("image/png");
+};
+
+const init = () => {
+  canvas = document.getElementById("canvas");
+  rawImage = document.getElementById("canvasimg");
+  ctx = canvas.getContext("2d");
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, 280, 280);
+  canvas.addEventListener("mousemove", draw);
+  canvas.addEventListener("mousedown", setPosition);
+  canvas.addEventListener("mouseenter", setPosition);
+  saveButton = document.getElementById("sb");
+  saveButton.addEventListener("click", save);
+  clearButton = document.getElementById("cb");
+  clearButton.addEventListener("click", erase);
+};
+
+const run = async () => {
+  const data = new MnistData();
+  await data.load();
+  const model = getModel();
+  tfvis.show.modelSummary({ name: "Model Architecture" }, model);
+  await train(model, data);
+  init();
+  alert("Training is done, try classifying your handwriting!");
+};
+
+document.addEventListener("DOMContentLoaded", run);
